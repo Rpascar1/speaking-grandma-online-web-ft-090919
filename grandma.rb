5d7285bba63a77a13e979df100296fe047873fbf
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-10 04:01:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 
 def speak_to_grandma(string)
-  if "#{string}" == "I LOVE YOU GRANDMA!" 
+  if "#{string}".upcase == "I LOVE YOU GRANDMA!" 
     puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
@@ -8,14 +8,25 @@ end
   
 
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
+describe '#speak_to_grandma' do
+  it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
+    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
+  end
 
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
+  it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
+    expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
+  end
 
-# NO, NOT SINCE 1938!
+  it 'responds with HUH?! SPEAK UP, SONNY! for a second time' do
+    expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
+  end
 
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+  it 'responds with NO, NOT SINCE 1938! when she can hear you' do
+    expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')).to eq "NO, NOT SINCE 1938!"
+  end
+
+  it 'responds with NO, NOT SINCE 1938! for a second time' do
+    expect(speak_to_grandma('WHAT?')).to eq "NO, NOT SINCE 1938!"
+  end
+
+end
