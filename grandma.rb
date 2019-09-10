4e3c14264f8fb090cb8ac9cef3e94bed6104a7c1
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-10 03:34:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,11 @@
 def speak_to_grandma(string)
-  if string.upcase
+  if string.upcase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif
     puts "NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
+    
   end
 end
 
